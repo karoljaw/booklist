520c0ab312ea8bdd6f244b1c3741636e7bfd88d9
Karol Jaworek <karoljaworek04@gmail.com>
2019-10-03 17:34:05 +0200

    fields validation, alerts, and delete book function added

--- app.js
+++ app.js
@@ -10,23 +10,33 @@ function UI() {}
 
   UI.prototype.addBook = function(book) {
     let list = document.getElementById("book-list");
-
     const row = document.createElement("tr");
 
     row.innerHTML = `
-          <th>${book.title}</th>
-          <th>${book.author}</th>
-          <th>${book.isbn}</th>
-          <th><a href="#">x</a></th>
-          `;
+      <th>${book.title}</th
+      <th>${book.author}</th>
+      <th>${book.isbn}</th>
+      <th><a href="#" class="form-link">x</a></th>
+      `;
 
-          list.appendChild(row);
-
+      list.appendChild(row);
   };
   UI.prototype.clearFields = function() {
     let title = document.getElementById("title").value = ''; 
     let author = document.getElementById("author").value = '';
     let isbn = document.getElementById("isbn").value = '';
+  }
+  UI.prototype.showMessage = function(message, status) {
+    let container = document.querySelector(".container");
+    let form = document.getElementById("book-form");
+    let info = document.createElement("div");
+    info.textContent = message;
+    info.classList.add(status); 
+    container.insertBefore(info, form);
+
+    setTimeout(function(){ 
+      info.remove(); 
+    }, 3000);
   }
 
 
@@ -40,9 +50,20 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
   const book = new Book(title, author, isbn);
 
   const ui =new UI();
-  ui.addBook(book);
-
-  ui.clearFields();
-
+  if (book.title == "" || book.author == ""|| book.isbn == "") {
+    ui.showMessage("Fill all fields", "error");
+  } else {
+    ui.addBook(book);
+    ui.clearFields();
+    ui.showMessage("Book added to the list", "success");
+  }
+  
   e.preventDefault();
-})
+});
+
+  document.getElementById("book-list").addEventListener("click", function(e) {
+  let link = document.querySelector(".form-link");
+  if (e.target === link) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
